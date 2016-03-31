17114cb507b98f09569b1f5d4b0ce51caab74297
PinkDeer <bunman@mail.ru>
2016-04-01 01:06:08 +0300

    Edit words/index and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 30.times do
   word = Word.create 	:title => "#{Faker::Lorem.word}",
-                        :translation => "#{Faker::Lorem.paragraph}"
+                        :translation => "#{Faker::Lorem.paragraph}",
+                        :user_id => "#{1}"
+
   puts word.inspect
 end
